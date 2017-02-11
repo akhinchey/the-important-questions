897133b66da1ad73eb85d2ea52046fd7dac4efa3
Amanda Hinchey <akhinchey@gmail.com>
2017-02-11 16:29:16 -0500

    Test back-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :questions
-  
+  resources :quizzes do
+    resources :questions do
+      resources :options
+    end
+  end
+
   root 'welcome#index'
 end
