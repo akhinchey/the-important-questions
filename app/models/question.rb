6d88023f5178e3375b9709983aa300d0973bd04d
Amanda Hinchey <akhinchey@gmail.com>
2017-02-11 15:09:00 -0500

    Add option to questions.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
 
   belongs_to :quiz, presence: true
   has_many :options, presence: true
-  has_one :answer
+  has_one :answer, foreign_key: "option_id"
 
 end
--- db/migrate/20170211195202_create_options.rb
+++ db/migrate/20170211195202_create_options.rb
@@ -2,6 +2,9 @@ class CreateOptions < ActiveRecord::Migration[5.0]
   def change
     create_table :options do |t|
 
+      t.integer :question_id, null: false
+      t.text :text, null: false
+
       t.timestamps
     end
   end
