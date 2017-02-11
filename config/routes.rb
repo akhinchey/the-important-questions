a3cc73cb88d9a50fd6ab5823a7ccc16059ab2874
Olivia Coffaro <ofcoffaro@gmail.com>
2017-02-11 17:04:45 -0500

    fix homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :quizzes 
-  
+  resources :quizzes
+
   post "quizzes" => "quizzes#create"
 
-  root 'welcome#index'
+  root 'quizzes#index'
 end
