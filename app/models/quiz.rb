b40309992d748c9280085a3eb984cc43310687a8
Amanda Hinchey <akhinchey@gmail.com>
2017-02-11 14:52:22 -0500

    created options model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,7 +3,26 @@ class Quiz < ApplicationRecord
   def initialize
 
     questions = [
-                Question.create(),
+                Question.create(content: "If you were a cocktail what would you be?",
+                  ),
+                Question.create(content: "What do you do to unwind?",
+                  ),
+                Question.create(content: "What is your ideal party?",
+                  ),
+                Question.create(content: "If you were a programming language, what would you be?",
+                  ),
+                Question.create(content: "Who is your celebrity crush?",
+                  ),
+                Question.create(content: "If you have a time machine what era would you go back to?",
+                  ),
+                Question.create(content: "What is your spirit animal?",
+                  ),
+                Question.create(content: "What did you want to be when you were a kid?",
+                  ),
+                Question.create(content: "What mythical creature do you fear most?",
+                  ),
+                Question.create(content: "What Hogwarts house do you belong to?",
+                  )
                 ]
 
   end
