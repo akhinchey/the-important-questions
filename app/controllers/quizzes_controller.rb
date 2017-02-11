da964ebc4b3a8d31cda4728264f314e2da3df2b0
Olivia Coffaro <ofcoffaro@gmail.com>
2017-02-11 18:19:49 -0500

    more quiz data

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -25,6 +25,55 @@ class QuizzesController < ApplicationController
         question_3.options.create(text: "Wine and cheese")
         question_3.options.create(text: "Party of one")
 
+    question_4 = @quiz.questions.create(content: "If you were a programming language, what would you be?")
+
+        question_4.options.create(text: "Java")
+        question_4.options.create(text: "Ruby")
+        question_4.options.create(text: "Go")
+        question_4.options.create(text: "HTML")
+
+    question_5 = @quiz.questions.create(content: "Who is your celebrity crush?")
+
+        question_5.options.create(text: "Oprah")
+        question_5.options.create(text: "Tom Hanks")
+        question_5.options.create(text: "Zac Efron")
+        question_5.options.create(text: "Scarlet Johansson ")
+
+    question_6 = @quiz.questions.create(content: "If you have a time machine what era would you go back to?")
+
+        question_6.options.create(text: "Chicago in the roaring twenties")
+        question_6.options.create(text: "Time travel isn’t real")
+        question_6.options.create(text: "Ancient Rome")
+        question_6.options.create(text: "Iowa in the 70s")
+
+    question_7 = @quiz.questions.create(content: "What is your spirit animal?")
+
+        question_7.options.create(text: "Unicorn")
+        question_7.options.create(text: "Sloth")
+        question_7.options.create(text: "Otter")
+        question_7.options.create(text: "Flamingo")
+
+    question_8 = @quiz.questions.create(content: "What did you want to be when you were a kid?")
+
+        question_8.options.create(text: "Famous")
+        question_8.options.create(text: "Philosopher")
+        question_8.options.create(text: "Fashion Designer")
+        question_8.options.create(text: "Paleontologist")
+
+      question_9 = @quiz.questions.create(content: "What mythical creature do you fear most?")
+
+          question_9.options.create(text: "Loch ness monster")
+          question_9.options.create(text: "Sasquatch")
+          question_9.options.create(text: "Sphinx")
+          question_9.options.create(text: "Dementor")
+
+      question_10 = @quiz.questions.create(content: "What Hogwarts house do you belong to?")
+
+          question_10.options.create(text: "Slytherin")
+          question_10.options.create(text: "Hufflepuff")
+          question_10.options.create(text: "Ravenclaw")
+          question_10.options.create(text: "Gryffindor")
+
         redirect_to quiz_path(@quiz)
   end
 
@@ -33,6 +82,7 @@ class QuizzesController < ApplicationController
   end
 
   def show
+    @quiz = Quiz.find(params[:id])
   end
 
 end
